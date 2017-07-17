19fca6ea1b7f17cf1f8957b9a55f39c6b46ea015
937989499 <937989499@qq.com>
2017-07-17 12:42:12 +0800

    first

---
+++ index.php
@@ -0,0 +1,461 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+	<title>新浪新闻</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+<div class="zhuyao">
+
+	<div class="header">
+		<div class="header_top">
+			<div class="top_left">
+				<img src="img/news_logo.png" style="height:40px;">
+			</div>
+			<div class="top_center">
+				<img src="img/l_09.png" style="margin-bottom:-3px;height:19px;"><span style="font-size:15px;">新浪新闻客户端</span>
+				<select style="border: 1px solid #e5e5e5;background-color: #f0e9e999;margin-left:20px;">
+					<option value="1">更多</option>
+				</select>
+			</div>
+			<div class="top_right">
+				<img src="img/l_06.png" style="margin-left:100px;height: 13px;margin-bottom: -2px;"><span style="font-size:13px;margin-right:15px;margin-left:8px;">设为书签</span>
+				<img src="img/l_03.png" style="height: 14px;margin-bottom: -2px;"><span style="font-size:13px;margin-left:8px;">手机新浪网</span>
+			</div>
+		</div>
+		<div class="clear"></div>
+		<div class="header_bottom">
+			<div class="header_list">
+				<ul>
+					<li class="now"><a href="">滚动</a></li>
+					<li><a href="">直播</a></li>
+					<li><a href="">调查</a></li>
+					<li><a href="">排行</a></li>
+					<li><a href="">国内</a></li>
+					<li><a href="">国际</a></li>
+					<li><a href="">社会</a></li>
+					<li><a href="">军事</a></li>
+					<li><a href="">评论</a></li>
+					<li><a href="">智库</a></li>
+					<li><a href="">政务</a></li>
+					<li><a href="">航拍</a></li>
+					<li><a href="">VR视频</a></li>
+					<li><a href="">图片</a></li>
+					<li><a href="">视频</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="main">
+		<div class="m_left">
+			<div class="ml2">
+				<div class="ml2_1">
+					<ul>
+						<li><a href="">热点</a></li>
+						<li class="now2"><a href="">聚焦</a></li>
+						<li class="now2"><a href="">天下</a></li>
+						<li class="now2"><a href="">奇趣</a></li>
+						<li class="now2"><a href="">军事</a></li>
+					</ul>
+				</div>
+				<div class="ml2_2">
+					<ul>
+						<li><a href=""><img src="img/26.png" style="width:298px;height:222px;"></a></li>
+					</ul>
+
+					<ul style="display:none;">
+						<li><a href=""><img src="img/27.jpg" style="width:298px;height:222px;"></a></li>
+					</ul>
+
+					<ul style="display:none;">
+						<li><a href=""><img src="img/26.png" style="width:298px;height:222px;"></a></li>
+					</ul>
+
+					<ul style="display:none;">
+						<li><a href=""><img src="img/27.jpg" style="width:298px;height:222px;"></a></li>
+					</ul>
+					<ul style="display:none;">
+						<li><a href=""><img src="img/26.png" style="width:298px;height:222px;"></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="ml3">
+				<div class="ml3_1">
+					<div class="ml31_news"><a href="">新闻放映厅</a></div>
+					<div class="ml31_more"><span><a href="">更多<img src="img/29.png"></a></span></div>
+				</div>
+				<ul class="ml31_ul">
+					<li>
+						<div>
+						<div class="ml31_img"><a href=""><img src="img/news1.jpg" style="width:150px;height:90px;"></a></div>
+						<div class="ml31_txt"><h3><a href="">GOOD TECH系列直播</a></h3>
+							<p><a href="">VR游戏也能开启组队模式?</a></p></div></div>
+							<div class="clear"></div>
+					</li>
+					<li>
+						<div>
+						<div class="ml31_img"><a href=""><img src="img/new2.jpg" style="width:150px;height:90px;"></a></div>
+						<div class="ml31_txt"><h3><a href="">GOOD TECH系列直播</a></h3>
+							<p><a href="">VR游戏也能开启组队模式?</a></p></div></div>
+							<div class="clear"></div>
+					</li>
+					<li>
+						<div>
+						<div class="ml31_img"><a href=""><img src="img/new3.png" style="width:150px;height:90px;"></a></div>
+						<div class="ml31_txt"><h3><a href="">GOOD TECH系列直播</a></h3>
+							<p><a href="">VR游戏也能开启组队模式?</a></p></div></div>
+							<div class="clear"></div>
+					</li>
+				</ul>
+			</div>
+			<div class="ml4">
+				<div class="ml4_1">
+					<div class="ml41_news"><a href="">新闻图片</a></div>
+					<div class="ml41_more"><span><a href="">更多<img src="img/29.png"></a></span></div>
+				</div>
+				<div class="ml4_2">
+					<ul>
+						<li><a href=""><img src="img/41.jpg" style="width:145px;height:90px;"></a><span class="ml42_txt">真人版芭比娃娃素颜曝光</span></li>
+						<li><a href=""><img src="img/42.jpg" style="width:145px;height:90px;"></a><span class="ml42_txt">真人版芭比娃娃素颜曝光</span></li>
+						<li><a href=""><img src="img/43.jpg" style="width:145px;height:90px;"></a><span class="ml42_txt">真人版芭比娃娃素颜曝光</span></li>
+						<li><a href=""><img src="img/44.jpg" style="width:145px;height:90px;"></a><span class="ml42_txt">真人版芭比娃娃素颜曝光</span></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="m_center">
+			<div class="mc_top">
+				<div class="mct_l">要闻</div>
+				<div class="mct_r">北京时间：2017.4.27 周四</div>
+			</div>
+			<div class="mc_cen">
+				<h1 class="mcc_h1"><a href="">习近平强调维护金融安全 提出6项任务</a></h1>
+				<h1 class="mcc_h1"><a href="">李克强：攻克雾霾成因 该花多少钱就花多少</a></h1>
+				<h1 class="mcc_h1"><a href="">航母下水和萨德入韩同天是计划？中方回应</a></h1>
+				<p class="mcc_p">
+					[<a href="">中国军方强硬回应：中俄将采取措施</a>
+					<a href="">韩美部署萨德遭民众反抗</a>]
+				</p>
+				<h1 class="mcc_h1"><a href="">中纪委一个月打虎6只 系十八大以来第二次</a></h1>
+				<p class="mcc_p">
+					[<a href="">再次秒杀老虎 安徽60后副省长周春雨被查</a>
+					<a href="">其落马一举创下3个最</a>]
+				</p>
+				<h1 class="mcc_h1"><a href="">公安部民政部提醒此事 和很多人钱袋子有关</a></h1>
+			</div>
+			<div class="mc_bot">
+					<ul class="mcb_ul">
+						<li class="mcb_li"><a href="">中国最新型枭龙战机首飞成功 已获国外订单</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+					</ul>
+					<div class="line_1"></div>
+					<ul class="mcb_ul">
+						<li class="mcb_li"><a href="">中国最新型枭龙战机首飞成功 已获国外订单</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+					</ul>
+					<div class="line_1"></div>
+					<ul class="mcb_ul">
+						<li class="mcb_li"><a href="">中国最新型枭龙战机首飞成功 已获国外订单</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+					</ul>
+					<div class="line_1"></div>
+					<ul class="mcb_ul">
+						<li class="mcb_li"><a href="">中国最新型枭龙战机首飞成功 已获国外订单</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐任人社部副部长</a></li>
+						<li><a href="">国务院任命多名副部官员:吴道槐</a><a href="" class="more_m">更多要闻></a></li>
+					</ul>
+
+			</div>
+			
+		</div>
+		<div class="m_right">
+			<div class="mr_1">
+				<div class="mr1_top">
+					<ul class="mr1t_ul">
+						<li class="select">观点</li>
+						<li>新浪智库</li>
+					</ul>
+				</div>
+
+				<div id="mr1_f1" class="right1">
+					<div class="mr1f1_1">
+						<div class="mr1f11_top">
+							<div class="s_sj">
+								<a href="javascript:;" class="dis"></a>
+								<a href="javascript:;" class="nxt"></a>
+							</div>
+							<a href="" class="top_ca">新浪政务</a>
+						</div>
+						<div class="itemwep" style="height:165px;">
+							<div class="item" id="item1" >
+								<div class="tl"><a href="">快递垃圾成污染大户 如何解决回收难</a></div>
+								<div class="onlines">
+									<div class="thumb"><a href=""><img src="img/r1.jpg" style="width:135px;height:135px;"></a></div>
+									<div class="txt">网购已成为人们生活的一部分。与之相伴的是，快递垃圾剧增。若处理不当，带来的污染将十分严重。<a href="">[详细]</a></div>
+								</div>
+							</div>
+							<div class="item" id="item2" style="display:none;">
+								<div class="tl"><a href="">环保部:京津冀多“散乱污”企业集群</a></div>
+								<div class="onlines">
+									<div class="thumb"><a href=""><img src="img/r2.jpg" style="width:135px;height:135px;"></a></div>
+									<div class="txt">部分小企业聚集于农村地区等环境复杂区，形成“散乱污”企业集群，成大气污染防治中的突出问题。<a href="">[详细]</a></div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="mr1f1_2">
+						<div class="mf_top">
+							<div class="s_sf" style="display:none;">
+								<a href="javascript:;" class="dis"></a>
+								<a href="javascript:;" class="nxt"></a>
+							</div>
+							<a href="" class="mft_head">锐见</a>
+						</div>
+						<div class="itemwep" style="width:600px;height:430px;">
+							<div class="item">
+								<div class="tl"><a href="">快递垃圾成污染大户 如何解决回收难</a></div>
+								<div class="onlines">
+									<div class="thumb"><a href=""><img src="img/r2.jpg" style="width:135px;height:135px;"></a></div>
+									<div class="txt">部分小企业聚集于农村地区等环境复杂区，形成“散乱污”企业集群，成大气污染防治中的突出问题。<a href="">[详细]</a></div>
+								</div>
+								<div class="tl"><a href="">环保部:京津冀多“散乱污”企业集群</a></div>
+								<div class="onlines">
+									<div class="thumb"><a href=""><img src="img/r1.jpg" style="width:135px;height:135px;"></a></div>
+									<div class="txt">网购已成为人们生活的一部分。与之相伴的是，快递垃圾剧增。若处理不当，带来的污染将十分严重。<a href="">[详细]</a></div>
+								</div>
+								<ul class="view_list">
+									<li>
+										<a href="">用上大学来衡量上升通道，有点刻舟求剑</a>
+									</li>
+									<li>
+										<a href="">用上大学来衡量上升通道，有点刻舟求剑</a>
+									</li>
+									<li>
+										<a href="">用上大学来衡量上升通道，有点刻舟求剑</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<div id="mr1_f2" class="right1" style="display:none;">
+					<div class="mr1f1_1">
+						<div class="mr1f11_top">
+							<div class="s_sj">
+								<a href="javascript:;" class="dis"></a>
+								<a href="javascript:;" class="nxt"></a>
+							</div>
+							<a href="" class="top_ca">智库热点</a>
+						</div>
+						<div class="itemwep" style="height:165px;">
+							<div class="item" id="item5" >
+								<div class="tl"><a href="">“一带一路”智库联盟会议在京召开</a></div>
+								<div class="onlines">
+									<div class="thumb"><a href=""><img src="img/r1.jpg" style="width:135px;height:135px;"></a></div>
+									<div class="txt">网购已成为人们生活的一部分。与之相伴的是，快递垃圾剧增。若处理不当，带来的污染将十分严重。<a href="">[详细]</a></div>
+								</div>
+							</div>
+							<div class="item" id="item6" style="display:none;">
+								<div class="tl"><a href="">“一带一路”智库联盟会议在京召开</a></div>
+								<div class="onlines">
+									<div class="thumb"><a href=""><img src="img/r2.jpg" style="width:135px;height:135px;"></a></div>
+									<div class="txt">部分小企业聚集于农村地区等环境复杂区，形成“散乱污”企业集群，成大气污染防治中的突出问题。<a href="">[详细]</a></div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="mr1f1_2">
+						<div class="mf_top">
+							<div class="s_sf" style="display:none;">
+								<a href="javascript:;" class="dis"></a>
+								<a href="javascript:;" class="nxt"></a>
+							</div>
+							<a href="" class="mft_head">专家观点</a>
+						</div>
+						<div class="itemwep" style="height:430px;">
+							<div class="item" id="item3">
+								<div class="tl"><a href="">快递垃圾成污染大户 如何解决回收难</a></div>
+								<div class="onlines">
+									<div class="thumb"><a href=""><img src="img/r2.jpg" style="width:135px;height:135px;"></a></div>
+									<div class="txt">网购已成为人们生活的一部分。与之相伴的是，快递垃圾剧增。若处理不当，带来的污染将十分严重。<a href="">[详细]</a></div>
+								</div>
+								<div class="tl"><a href="">环保部:京津冀多“散乱污”企业集群</a></div>
+								<div class="onlines">
+									<div class="thumb"><a href=""><img src="img/r1.jpg" style="width:135px;height:135px;"></a></div>
+									<div class="txt">部分小企业聚集于农村地区等环境复杂区，形成“散乱污”企业集群，成大气污染防治中的突出问题。<a href="">[详细]</a></div>
+								</div>
+								<ul class="view_list">
+									<li>
+										<a href="">有点刻舟求剑，用上大学来衡量上升通道</a>
+									</li>
+									<li>
+										<a href="">有点刻舟求剑，用上大学来衡量上升通道</a>
+									</li>
+									<li>
+										<a href="">有点刻舟求剑，用上大学来衡量上升通道</a>
+									</li>
+								</ul>
+							</div>
+							<div class="item" id="item4" style="display:none;">
+								<div class="tl"><a href="">如何解决回收难 快递垃圾成污染大户</a></div>
+								<div class="onlines">
+									<div class="thumb"><a href=""><img src="img/r2.jpg" style="width:135px;height:135px;"></a></div>
+									<div class="txt">部分小企业聚集于农村地区等环境复杂区，形成“散乱污”企业集群，成大气污染防治中的突出问题。<a href="">[详细]</a></div>
+								</div>
+								<div class="tl"><a href="">环保部:京津冀多“散乱污”企业集群</a></div>
+								<div class="onlines">
+									<div class="thumb"><a href=""><img src="img/r1.jpg" style="width:135px;height:135px;"></a></div>
+									<div class="txt">网购已成为人们生活的一部分。与之相伴的是，快递垃圾剧增。若处理不当，带来的污染将十分严重。<a href="">[详细]</a></div>
+								</div>
+								<ul class="view_list">
+									<li>
+										<a href="">用上大学来衡量上升通道，有点刻舟求剑</a>
+									</li>
+									<li>
+										<a href="">用上大学来衡量上升通道，有点刻舟求剑</a>
+									</li>
+									<li>
+										<a href="">用上大学来衡量上升通道，有点刻舟求剑</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="banek_1">
+				<div class="bimg">
+					<a href=""><img src="img/yanjing.jpg" style="height:122px;"></a>
+					<a href=""><img src="img/yanjing.jpg" style="height:122px;"></a>
+				</div>
+			</div>
+		</div>
+	<div class="clear"></div>
+	</div>
+	<div class="footer">
+		<p>
+			24小时客户服务热线：4006900000 010-82623378
+			<a href="">常见问题解答</a> <a href="">互联网违法和不良信息举报</a> 
+		</p>
+		<p>违法和不良信息举报电话：010-62675637 举报邮箱：<a href="">jubao@vip.sina.com</a></p>
+		<p><a href="">新闻中心意见反馈留言板</a></p>
+		<p>
+			<a href="">新浪简介</a>|
+			<a href="">About Sina</a>|
+			<a href="">广告服务</a>|
+			<a href="">联系我们</a>|
+			<a href="">招聘信息</a>|
+			<a href="">网站律师</a>|
+			<a href="">SINA English</a>|
+			<a href="">通行证注册</a>|
+			<a href="">产品答疑</a>
+		</p>
+		<p>
+			Copyright © 1996-2017 SINA Corporation, All Rights Reserved
+		</p>
+		<p>
+			新浪公司 <a href="">版权所有</a>
+		</p>
+	</div>
+</div>
+<script type="text/javascript" src="js/jquery-1.7.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.header_list li').mouseover(function(){
+			$('.header_list li').removeClass("now");
+			$(this).addClass("now");
+		});
+		$('.ml2_1 li').mouseover(function(){
+			$('.ml2_1 li').addClass("now2");
+			$(this).removeClass("now2");
+			var i = $(this).index();
+			$('.ml2_2 ul').attr("style","display:none;");
+			$('.ml2_2 ul').eq(i).attr("style","display:block;");
+		});
+		$('.s_sj a.nxt').click(function()
+		{
+			$('#item2').show();
+			$('#item1').hide();
+			$('.s_sj a.dis').css("background-position","0 0px");
+			$('.s_sj a.dis').css("cursor","pointer");
+			$('.s_sj a.nxt').css("cursor","default");
+			$('.s_sj a.nxt').css("background-position","0 -130px")
+		});
+		$('.s_sj a.dis').click(function(){
+			$('#item1').show();
+			$('#item2').hide();
+			$('.s_sj a.dis').css("background-position","0 -104px");
+			$('.s_sj a.nxt').css("cursor","pointer");
+			$('.s_sj a.dis').css("cursor","default");
+			$('.s_sj a.nxt').css("background-position","0 -26px");
+		});
+
+		$('.s_sj a.nxt').click(function()
+		{
+			$('#item6').show();
+			$('#item5').hide();
+			$('.s_sj a.dis').css("background-position","0 0px");
+			$('.s_sj a.dis').css("cursor","pointer");
+			$('.s_sj a.nxt').css("cursor","default");
+			$('.s_sj a.nxt').css("background-position","0 -130px")
+		});
+		$('.s_sj a.dis').click(function(){
+			$('#item5').show();
+			$('#item6').hide();
+			$('.s_sj a.dis').css("background-position","0 -104px");
+			$('.s_sj a.nxt').css("cursor","pointer");
+			$('.s_sj a.dis').css("cursor","default");
+			$('.s_sj a.nxt').css("background-position","0 -26px");
+		});
+
+		$('.s_sf a.nxt').click(function()
+		{
+			$('#item4').show();
+			$('#item3').hide();
+			$('.s_sf a.dis').css("background-position","0 0px");
+			$('.s_sf a.dis').css("cursor","pointer");
+			$('.s_sf a.nxt').css("cursor","default");
+			$('.s_sf a.nxt').css("background-position","0 -130px")
+		});
+		$('.s_sf a.dis').click(function()
+		{
+			$('#item3').show();
+			$('#item4').hide();
+			$('.s_sf a.dis').css("background-position","0 -104px");
+			$('.s_sf a.nxt').css("cursor","pointer");
+			$('.s_sf a.dis').css("cursor","default");
+			$('.s_sf a.nxt').css("background-position","0 -26px")
+		});
+		$('.mr1t_ul li').mouseover(function(){
+			$('.mr1t_ul li').removeClass("select");
+			$(this).addClass("select");
+			var w = $(this).index();
+			$('.right1').attr("style","display:none;");
+			$('.right1').eq(w).attr("style","display:block;");
+			if (w==0) {
+				$('.s_sf').hide();
+			}
+			else{
+				$('.s_sf').show();
+			}
+		});
+	});
+</script>
+</body>
+</html>
